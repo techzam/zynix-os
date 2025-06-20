90a8656d10f47df23acd6fd5717723caba313717
techzam <suraj.uk.897@gmail.com>
2025-06-20 13:16:04 +0530

    Create main.dart
    Added lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Zynix OS',
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("🚀 Welcome to Zynix OS", style: TextStyle(fontSize: 20))),
+    );
+  }
+}

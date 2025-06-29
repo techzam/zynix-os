0fbff28d037ca30560fa95b10fd2be9f879b5872
techzam <suraj.uk.897@gmail.com>
2025-06-29 13:46:55 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,4 +1,14 @@
-import 'package:firebase_core/firebase_core.dart'; import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
 
-class DefaultFirebaseOptions { static FirebaseOptions get currentPlatform { return const FirebaseOptions( apiKey: "YOUR_API_KEY", appId: "YOUR_APP_ID", messagingSenderId: "YOUR_MESSAGING_SENDER_ID", projectId: "YOUR_PROJECT_ID", storageBucket: "YOUR_BUCKET.appspot.com", ); } }
-
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyDs8O7Uu2gHw_tJ5idSiIugjuIYFsedxMo",
+      appId: "1:682885435219:android:26bb4e2d75be60a6985ba6",
+      messagingSenderId: "682885435219",
+      projectId: "zynix-os",
+      storageBucket: "zynix-os.firebasestorage.app",
+    );
+  }
+}

0035a7cad45016d9d860b7532b2000b2b5af0505
techzam <suraj.uk.897@gmail.com>
2025-06-29 13:36:56 +0530

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,4 @@
+import 'package:firebase_core/firebase_core.dart'; import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+
+class DefaultFirebaseOptions { static FirebaseOptions get currentPlatform { return const FirebaseOptions( apiKey: "YOUR_API_KEY", appId: "YOUR_APP_ID", messagingSenderId: "YOUR_MESSAGING_SENDER_ID", projectId: "YOUR_PROJECT_ID", storageBucket: "YOUR_BUCKET.appspot.com", ); } }
+
